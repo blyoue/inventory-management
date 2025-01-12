d7563e4659660f802ca116a82e7ef1043d714a0e
blyoue <siraphop.siraboonchai@gmail.com>
2025-01-13 03:33:09 +0700

    added images

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,7 +1,18 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    /* config options here */
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname:
+                    's3-inventorymanagement-blue.s3.ap-southeast-1.amazonaws.com',
+                port: '',
+                pathname: '/**',
+            },
+        ],
+    },
 };
 
 export default nextConfig;
--- client/src/app/dashboard/CardPopularProducts.tsx
+++ client/src/app/dashboard/CardPopularProducts.tsx
@@ -1,11 +1,11 @@
-import { useGetDashboardMetricsQuery } from '@/state/api'
+import { useGetDashboardMetricsQuery } from '@/state/api';
 import { ShoppingBag } from 'lucide-react';
-import React from 'react'
+import React from 'react';
 import Rating from '../(components)/Rating';
+import Image from 'next/image';
 
 const CardPopularProducts = () => {
     const { data: dashboardMetrics, isLoading } = useGetDashboardMetricsQuery();
-
 
     return (
         <div className='row-span-3 xl:row-span-6 bg-white shadow-md rounded-2xl pb-16'>
@@ -24,23 +24,36 @@ const CardPopularProducts = () => {
                                 className='flex items-center justify-between gap-3 px-5 py-7 border-b'
                             >
                                 <div className='flex items-center gap-3'>
-                                    <div>img</div>
+                                    <Image
+                                        src={`https://s3-inventorymanagement-blue.s3.ap-southeast-1.amazonaws.com/product${
+                                            Math.floor(Math.random() * 3) + 1
+                                        }.png`}
+                                        alt={product.name}
+                                        width={48}
+                                        height={48}
+                                        className='rounded-lg w-14 h-14'
+                                    />
                                     <div className='flex flex-col justify-between gap-1'>
-                                        <div className='font-bold text-gray-700'>{product.name}</div>
+                                        <div className='font-bold text-gray-700'>
+                                            {product.name}
+                                        </div>
                                         <div className='flex text-sm items-center'>
                                             <span className='font-bold text-blue-500 text-xs'>
                                                 ${product.price}
                                             </span>
                                             <span className='mx-2'>|</span>
-                                            <Rating rating={product.rating || 0}/>
+                                            <Rating
+                                                rating={product.rating || 0}
+                                            />
                                         </div>
                                     </div>
                                 </div>
                                 <div className='text-xs felx items-center'>
                                     <button className='p-2 rounded-full bg-blue-100 text-blue-600 mr-2'>
-                                        <ShoppingBag className='w-4 h-4'/>
+                                        <ShoppingBag className='w-4 h-4' />
                                     </button>
-                                    {Math.round(product.stockQuantity / 1000)}k Sold
+                                    {Math.round(product.stockQuantity / 1000)}k
+                                    Sold
                                 </div>
                             </div>
                         ))}
@@ -48,7 +61,7 @@ const CardPopularProducts = () => {
                 </>
             )}
         </div>
-    )
-}
+    );
+};
 
-export default CardPopularProducts
+export default CardPopularProducts;
